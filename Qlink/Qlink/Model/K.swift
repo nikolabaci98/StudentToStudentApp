a9f4af7c4372e4c28bf4792a47616af409c61c9b
Nikola Baci <46806109+nikolabaci98@users.noreply.github.com>
2022-04-09 01:27:24 -0400

    signup and login screens completed

---
+++ Qlink/Qlink/Model/K.swift
@@ -0,0 +1,23 @@
+//
+//  K.swift
+//  Qlink
+//
+//  Created by Nikola Baci on 4/7/22.
+//
+
+import Foundation
+
+
+struct K {
+    static let SignupSegue = "Signup"
+    static let LoginSegue = "Login"
+    
+    static let emailToPasswordSegue = "EmailToPassword"
+    static let passwordToScreenNameSegue = "PasswordToScreenName"
+    static let signupToConfrimEmailSegue = "ConfirmEmail"
+    static let confirmEmailToHomeSegue = "EmailToHome"
+    
+    static let loginToHomeSegue = "LoginToHome"
+    
+    static let emailDomain = "@qmail.cuny.edu"
+}
